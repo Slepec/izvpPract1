d7116f0c66182a1e66e1aafe8e0d5936e7353e8c
Dmytro Sihov <dima.sigov1@gmail.com>
2021-09-26 13:57:03 +0300

    Добавьте файлы проекта.

---
+++ izvpPract1/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿
+namespace izvpPract1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.tb3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tbH = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tbW = new System.Windows.Forms.TextBox();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb1
+            // 
+            this.tb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tb1.Location = new System.Drawing.Point(418, 127);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(100, 26);
+            this.tb1.TabIndex = 0;
+            // 
+            // tb2
+            // 
+            this.tb2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tb2.Location = new System.Drawing.Point(418, 177);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(100, 26);
+            this.tb2.TabIndex = 1;
+            // 
+            // tb3
+            // 
+            this.tb3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tb3.Location = new System.Drawing.Point(418, 222);
+            this.tb3.Name = "tb3";
+            this.tb3.Size = new System.Drawing.Size(100, 26);
+            this.tb3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox4.Location = new System.Drawing.Point(634, 222);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox5.Location = new System.Drawing.Point(634, 177);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 26);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox6.Location = new System.Drawing.Point(634, 127);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 26);
+            this.textBox6.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(442, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(201, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Карамельки";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(201, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Льодяники";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(201, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(169, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Шоколадні цукерки";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.Location = new System.Drawing.Point(378, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(210, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Необхідно для 1 набору";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label6.Location = new System.Drawing.Point(631, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "В наявності";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button1.Location = new System.Drawing.Point(895, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 42);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Розрахувати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label7.Location = new System.Drawing.Point(312, 300);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(717, 129);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "До подарункового новорічного набору входять: А шоколадних цукерок, B\r\nльодяників " +
+    "та С карамельок. Визначити, скільки наборів можна створити,\r\nякщо є N шоколадних" +
+    " цукерок, M льодяників та L карамельок.";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(983, 433);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 50);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Автоматичне заповнення";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tbH
+            // 
+            this.tbH.Location = new System.Drawing.Point(1145, 356);
+            this.tbH.Name = "tbH";
+            this.tbH.Size = new System.Drawing.Size(161, 22);
+            this.tbH.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(980, 359);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 17);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Висота";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1361, 541);
+            this.tabControl1.TabIndex = 18;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.lblMax);
+            this.tabPage1.Controls.Add(this.label9);
+            this.tabPage1.Controls.Add(this.tbW);
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.tbH);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1353, 512);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Завдання 1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.tb1);
+            this.tabPage2.Controls.Add(this.tb2);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.tb3);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.textBox6);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.textBox5);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.textBox4);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1329, 488);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Завдання 3";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(980, 307);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(59, 17);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Ширина";
+            // 
+            // tbW
+            // 
+            this.tbW.Location = new System.Drawing.Point(1145, 304);
+            this.tbW.Name = "tbW";
+            this.tbW.Size = new System.Drawing.Size(161, 22);
+            this.tbW.TabIndex = 18;
+            // 
+            // lblMax
+            // 
+            this.lblMax.Location = new System.Drawing.Point(983, 29);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(180, 47);
+            this.lblMax.TabIndex = 20;
+            this.lblMax.Text = "Найбільший елемент на головній діагоналі: ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 17);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(915, 466);
+            this.dataGridView1.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1361, 541);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Сігов";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.TextBox tb3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbH;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tbW;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ izvpPract1/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace izvpPract1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int[,] array;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int max = Convert.ToInt32(textBox6.Text) / Convert.ToInt32(tb1.Text); ;
+                if (max > Convert.ToInt32(textBox5.Text) / Convert.ToInt32(tb2.Text)) max = Convert.ToInt32(textBox5.Text) / Convert.ToInt32(tb2.Text);
+                if (max > Convert.ToInt32(textBox4.Text) / Convert.ToInt32(tb3.Text)) max = Convert.ToInt32(textBox4.Text) / Convert.ToInt32(tb3.Text);
+                label1.Text = max.ToString();
+            }
+            catch (Exception exc) { MessageBox.Show(exc.Message); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.ColumnHeader;
+                int h = 0;
+                int w = 0;
+                Boolean check = true;
+                Random rnd = new Random();
+                if (tbH.Text == "" )
+                {
+                    h = 5;
+                }
+                else
+                {
+                    h = Convert.ToInt32(tbH.Text);
+
+                }
+                if (tbW.Text =="")
+                { w = 5; }
+                else {
+                    w = Convert.ToInt32(tbW.Text);
+                    
+                }
+                array = new int[h, w];
+                if (h != w) {
+                    check = false;
+                    lblMax.Text = "Головної діагоналі немає"; 
+                }
+                for (int i = 0; i < w; i++)
+                {
+                    dataGridView1.Columns.Add("Column"+i, "Column " + i);
+
+                }
+                for (int i = 0; i < h; i++)
+                {
+                    dataGridView1.Rows.Add();
+
+                }
+                for (int i = 0; i < h; i++)
+                {
+                  
+                    for (int j = 0; j < w; j++)
+                    {
+                        array[i,j]=  rnd.Next(1,50);
+                        dataGridView1.Rows[i].Cells[j].Value = array[i,j];
+                    }
+                }
+                dataGridView1.Rows.Add();
+                int mult = 1;
+                for (int i = 0; i < w; i++)
+                {
+                    mult = 1;
+                    for (int j = 0; j < h; j++)
+                    {
+                        mult *=Convert.ToInt32( dataGridView1.Rows[j].Cells[i].Value);
+
+                    }
+                    dataGridView1.Rows[dataGridView1.Rows.Count -2].Cells[i].Value = mult;
+                    dataGridView1.Rows[dataGridView1.Rows.Count - 2].Cells[i].Style.BackColor = Color.Red;
+
+                }
+                dataGridView1.Columns.Add("Sum","Sum");
+                int sum = 0;
+                
+                for (int i = 0; i < h; i++)
+                {
+                    sum = 0;
+                    for (int j = 0; j < w; j++)
+                    {       
+                        sum += Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value);
+                       
+                    }
+                    dataGridView1.Rows[i].Cells[dataGridView1.Columns.Count-1].Value = sum;
+                    dataGridView1.Rows[i].Cells[dataGridView1.Columns.Count - 1].Style.BackColor = Color.Red;
+
+                }
+                if(check)
+                {
+                    int max = 0;
+                    for (int i = 0; i < h; i++)
+                    {
+
+                        if(Convert.ToInt32( dataGridView1.Rows[i].Cells[i].Value) > max) max= Convert.ToInt32(dataGridView1.Rows[i].Cells[i].Value);
+
+                    }
+                    lblMax.Text += max;
+                }
+
+            }
+           
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
